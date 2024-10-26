bd3c43c23aa24369dde94bac90b4f1e07448cedd
NiPeGun <nipegun@gmail.com>
2024-10-26 17:21:53 +0200

    Update and rename OpenWrtX86-Instalar1.sh to OpenWrtX86-Instalar2.sh

--- InstSO/OpenWrtX86-Instalar2.sh
+++ InstSO/OpenWrtX86-Instalar2.sh
@@ -111,14 +111,16 @@ menu=(dialog --checklist "Instalación de OpenWrt X86:" 30 100 20)
           # Crear tabla de particiones GPT
             sudo parted -s $vPrimerDisco mklabel gpt
           # Crear la partición OVMF
-            sudo parted -s $vPrimerDisco mkpart OVMF ext4 1MiB 201MiB
+            #sudo parted -s $vPrimerDisco mkpart OVMF ext4 1MiB 201MiB
+            sudo parted -s $vPrimerDisco mkpart OVMF ext4 0MiB 1024MiB
           # Crear la partición ext4
             # Obtener la cantidad de espacio libre disonible para particionar
-              vCantEspacioLibre=$(parted "$vPrimerDisco" print free | grep ree | tail -n1 | sed 's-MB--g' | sed 's-  - -g' | sed 's-  - -g' | cut -d' ' -f5)
-              vCantEspacioAUsar=$((vCantEspacioLibre - 1024))
-            sudo parted -s "$vPrimerDisco" mkpart OpenWrt ext4 201MiB "$vCantEspacioAUsar"MiB
+              #vCantEspacioLibre=$(parted "$vPrimerDisco" print free | grep ree | tail -n1 | sed 's-MB--g' | sed 's-  - -g' | sed 's-  - -g' | cut -d' ' -f5)
+              #vCantEspacioAUsar=$((vCantEspacioLibre - 1024))
+            #sudo parted -s "$vPrimerDisco" mkpart OpenWrt ext4 201MiB "$vCantEspacioAUsar"MiB
+            sudo parted -s "$vPrimerDisco" mkpart OpenWrt ext4 1024MiB 3072MiB
           # Crear la partición de intercambio
-            sudo parted -s $vPrimerDisco mkpart Intercambio ext4 24580MiB 100%
+            sudo parted -s $vPrimerDisco mkpart Intercambio ext4 3072MiB 100%
 
         ;;
 
